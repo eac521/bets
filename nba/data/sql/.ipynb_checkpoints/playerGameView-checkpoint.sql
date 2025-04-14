d15cd2d66c53e7afa899d8efc7183ddaff139637
Eric Coxon <ericcoxon@Erics-Laptop.local>
2025-04-14 07:08:51 -0400

    2024-25 completed.  Updating sql lite queries to optimize

--- nba/data/sql/.ipynb_checkpoints/playerGameView-checkpoint.sql
+++ nba/data/sql/.ipynb_checkpoints/playerGameView-checkpoint.sql
@@ -2,6 +2,15 @@
 --     SELECT sum(abv_fga) as teamAbvFga, sum(crn_fga) as teamCrnFga, sum(ra_fga) as teamRaFga, sum(mid_fga) as teamMidFga, sum(paint_fga) as teamPaintFga, 
 CREATE VIEW pgames 
     AS
+WITH daysSince AS (
+    SELECT player_id, game_date,
+    julianday(lag(game_date,1) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game1_date,
+    julianday(lag(game_date,2) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game2_date,
+    julianday(lag(game_date,3) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game3_date,
+    julianday(lag(game_date,4) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game4_date,
+    julianday(lag(game_date,5) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game5_date
+    FROM plyrLogs
+    )
 SELECT 
 --identifiying information
 name, teamAbrv as team,season,tmGameCt,
@@ -16,16 +25,16 @@ home,plogs.*,
 --last games played
 julianday(game_date) - julianday(lag(game_date,1) OVER (PARTITION BY player_id,season ORDER BY game_date)) - 1 daysBetweenGames,
 (
-case when julianday(game_date) - julianday(lag(game_date,5) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 5 then 1 else 0 end +
-case when julianday(game_date) - julianday(lag(game_date,4) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 5 then 1 else 0 end + 
-case when julianday(game_date) - julianday(lag(game_date,3) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 5 then 1 else 0 end +
-case when julianday(game_date) - julianday(lag(game_date,2) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 5 then 1 else 0 end +
-case when julianday(game_date) - julianday(lag(game_date,1) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 5 then 1 else 0 end) + 1 
+case when julianday(game_date) - game1_date < 5 then 1 else 0 end +
+case when julianday(game_date) - game2_date < 5 then 1 else 0 end + 
+case when julianday(game_date) - game3_date < 5 then 1 else 0 end +
+case when julianday(game_date) - game4_date < 5 then 1 else 0 end +
+case when julianday(game_date) - game5_date < 5 then 1 else 0 end) + 1 
     gamesInFive,
 (
-case when julianday(game_date) - julianday(lag(game_date,3) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 3 then 1 else 0 end +
-case when julianday(game_date) - julianday(lag(game_date,2) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 3 then 1 else 0 end +
-case when julianday(game_date) - julianday(lag(game_date,1) OVER (PARTITION BY player_id,season ORDER BY game_date)) < 3 then 1 else 0 end) + 1 gamesInThree,
+case when julianday(game_date) - game3_date < 3 then 1 else 0 end +
+case when julianday(game_date) - game2_date < 3 then 1 else 0 end +
+case when julianday(game_date) - game1_date < 3 then 1 else 0 end) + 1 gamesInThree,
 
 --threes made
 coalesce(lc_fgm,0) + coalesce(rc_fgm,0) + coalesce(abv_fgm,0) as threesMade,
@@ -53,6 +62,7 @@ LEFT JOIN
     RANK() OVER(PARTITION BY team_id,season ORDER BY game_date) tmGameCt 
     from teamLog) tm USING (team_id,game_id) 
 LEFT JOIN teams tms USING (team_id)
+LEFT JOIN daysSince ds USING (player_id,game_date)
 LEFT join players ply USING (player_id)
 LEFT JOIN 
 --get opponent shot profile
--- nba/data/sql/teamDView.sql
+++ nba/data/sql/teamDView.sql
@@ -1,32 +1,49 @@
 
 CREATE VIEW team_def AS
+    WITH grpdShots AS (
+    SELECT team_id, game_id, game_date,
+    CAST(lc_fga + rc_fga + abv_fga AS FLOAT) as threes_fga,
+    CAST(mid_fga + ra_fga + paint_fga AS FLOAT) as twos_fga
+    FROM shotsAllowed
+)
+
+WITH daysSince AS (
+    SELECT player_id, game_date,
+    julianday(lag(game_date,1) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game1_date,
+    julianday(lag(game_date,2) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game2_date,
+    julianday(lag(game_date,3) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game3_date,
+    julianday(lag(game_date,4) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game4_date,
+    julianday(lag(game_date,5) OVER (PARTITION BY player_id,season ORDER BY game_date)) as game5_date
+    FROM teamLog
+    )
 select 
 season, substr(sht.game_date,6,2) as month, teamAbrv as team, rank() OVER(PARTITION BY team_id,season ORDER BY sht.game_date) game_number,
 --last games played
 julianday(sht.game_date) - julianday(lag(sht.game_date,1) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) - 1 daysBetweenGames,
 (
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,5) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 5 then 1 else 0 end +
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,4) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 5 then 1 else 0 end + 
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,3) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 5 then 1 else 0 end +
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,2) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 5 then 1 else 0 end +
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,1) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 5 then 1 else 0 end) + 1 games_in_five,
+case when julianday(log.game_date) - game5_date < 5 then 1 else 0 end +
+case when julianday(log.game_date) - game4_date < 5 then 1 else 0 end + 
+case when julianday(log.game_date) - game3_date < 5 then 1 else 0 end +
+case when julianday(log.game_date) - game2_date < 5 then 1 else 0 end +
+case when julianday(log.game_date) - game1_date < 5 then 1 else 0 end) + 1 games_in_five,
 (
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,3) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 3 then 1 else 0 end +
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,2) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 3 then 1 else 0 end +
-case when julianday(sht.game_date) - julianday(lag(sht.game_date,1) OVER (PARTITION BY team_id,season ORDER BY sht.game_date)) < 3 then 1 else 0 end) + 1 games_in_three,
+case when julianday(log.game_date) - game3_date < 3 then 1 else 0 end +
+case when julianday(log.game_date) - game2_date < 3 then 1 else 0 end +
+case when julianday(log.game_date) - game1_date < 3 then 1 else 0 end) + 1 games_in_three,
 open_fg3a,
 wide_fg3a,
 --open shot rates    
-open_fg3a / (CAST(lc_fga as FLOAT) + CAST(rc_fga AS FLOAT) + CAST(abv_fga AS FLOAT)) as open3_rate,
-wide_fg3a / (CAST(lc_fga as FLOAT) + CAST(rc_fga AS FLOAT) + CAST(abv_fga AS FLOAT)) as wide3_rate,
-open_fg2a / (CAST(mid_fga as FLOAT) + CAST(ra_fga AS FLOAT) + CAST(paint_fga AS FLOAT)) as open2_rate,
-wide_fg2a / (CAST(mid_fga as FLOAT) + CAST(ra_fga AS FLOAT) + CAST(paint_fga AS FLOAT)) as wide2_rate,
+open_fg3a / threes_fga as open3_rate,
+wide_fg3a / threes_fga as wide3_rate,
+open_fg2a / twos_fga as open2_rate,
+wide_fg2a / twos_fga as wide2_rate,
 
 count_inactive,def_rate,pace,
 round(pace  * def_rate / 100) as points_allowed,
-win, home, sht.*,crn_fga + abv_fga threes_fga
+win, home, sht.*, threes_fga
 from  shotsAllowed sht
 JOIN teamLog log USING (team_id,game_id)
+JOIN daysSince ds using (team_id,g
 JOIN teams tms USING (team_id)
 left join (select team_id,game_id,
 dreb,dreb_contest,dreb_chances,dreb_chance_defer,avg_dreb_dist,pf
