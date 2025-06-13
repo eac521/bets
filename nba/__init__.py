ea24fa23151bc952ecdd1fcba5ad59c32146dfb0
eac521 <50027931+eac521@users.noreply.github.com>
2025-06-12 21:37:15 -0400

    Modularized the module, splitting into: base, etl, data, models and funcs in a betting module.  Also, updated in notebook to work through odds api data and saving into newly created tables for odds -- this needs updated and vetted --.  We will need to update from the macbook as well.  Need to save down the database to have updates matching across.

---
+++ nba/__init__.py
@@ -0,0 +1,5 @@
+from .nba_base import NBABase
+from .nba_data_fetcher import NBADataFetcher
+from .nba_data_processor import NBADataProcessor
+from .nba_betting_model import NBABettingModel
+from .nba_betting_system import NBABettingSystem
